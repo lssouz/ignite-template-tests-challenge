edf94c3be1a4ee72ee7bff41b36292a4226e4c08
Leo S <lssouz@icloud.com>
2023-02-01 17:52:11 -0300

    Create User

---
+++ README.md
@@ -0,0 +1,9 @@
+- [X] [POST] /users should be able to create new users
+- [X] [POST] /users should not be able to create new users when email is already token
+- [ ] [PATCH] /users/:user_id/admin should be able to turn an user as admin
+- [ ] [PATCH] /users/:user_id/admin should not be able to turn a non existing user as admin
+- [ ] [GET] /users/:user_id should be able to get user profile by ID
+- [ ] [GET] /users/:user_id should not be able to show profile of a non existing user
+- [ ] [GET] /users should be able to list all users
+- [ ] [GET] /users should not be able to a non admin user get list of all users
+- [ ] [GET] /users should not be able to a non existing user get list of all users
---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase"
+import { ICreateUserDTO } from "./ICreateUserDTO";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create User", () => {
+    beforeEach(() => {
+      inMemoryUsersRepository = new InMemoryUsersRepository();
+      createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    })
+
+    it("should be able to create new users", async () => {
+      const user = await createUserUseCase.execute({
+        name: "Leonardo",
+        password: "123456",
+        email: "teste@testes.com"
+      });
+
+      const findUser = await inMemoryUsersRepository.findByEmail(user.email);
+
+      expect(findUser).toHaveProperty("id");
+    })
+
+    it ("should not be able to create new users when email is already token", async () => {
+      const user: ICreateUserDTO = {
+        name: "Leonardo",
+        password: "123456",
+        email: "teste@testes.com"
+      }
+
+      expect(async () => {
+        await createUserUseCase.execute(user);
+        await createUserUseCase.execute(user);
+      }).rejects.toBeInstanceOf(AppError);
+    });
+})
